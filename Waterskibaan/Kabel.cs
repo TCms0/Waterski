4b738f6c40a542bc30c0d0cf256c8b48e42af3c0
Tristan <Tristan15015@gmail.com>
2019-09-24 17:51:58 +0200

    files

---
+++ Waterskibaan/Kabel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Kabel
+    {
+
+        LinkedList<Lijn> _lijnen = new LinkedList<Lijn>();
+
+
+        public Boolean IsStartPositieLeeg()
+        {
+            
+            if (_lijnen.AddFirst == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void VerschuifLijnen()
+        {
+
+        }
+    }
+}
---
+++ Waterskibaan/Sporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waterskibaan
+{
+    class Sporter
+    {
+        public int AantalRondenNogTeGaan
+        {
+            get { return AantalRondenNogTeGaan; }
+            set { AantalRondenNogTeGaan = 0; }
+        }
+        public Zwemvest Zwemvest
+        {
+            get {return Zwemvest; }
+            set {Console.WriteLine("Hekkie"); }
+        }
+        public Skies skies { get; set; }
+        public Color KledingKleur { get; set; }
+        public List<IMoves> moves { get; set; }
+
+        public Sporter(List<IMoves> moves)
+        {
+            for (int x = 0; x < moves.Count; x++)
+            {
+                Console.WriteLine(moves[x]);
+            }
+        }
+
+
+    }
+}
