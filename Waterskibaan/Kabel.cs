ed0a26ac20053b3cfebb9af1037dc05a6e54ddc5
Tristan <Tristan15015@gmail.com>
2019-09-26 15:04:10 +0200

    toevoeging

--- Waterskibaan/Kabel.cs
+++ Waterskibaan/Kabel.cs
@@ -14,20 +14,70 @@ namespace Waterskibaan
 
         public Boolean IsStartPositieLeeg()
         {
-            
-            if (_lijnen.AddFirst == 0)
+            if (_lijnen.Count != 0)
             {
-                return true;
+                if (_lijnen.First.Value.PositieOpDeLijn == 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
             }
             else
             {
-                return false;
+                return true;
             }
         }
 
+        public void NeemLijnInGebruik(Lijn lijn){
+           if (IsStartPositieLeeg())
+            {
+                lijn.PositieOpDeLijn = 0;
+                _lijnen.AddFirst(lijn);
+            }
+            else
+            {
+                Console.WriteLine("Positie is vol!");
+            }
+         }
+
         public void VerschuifLijnen()
         {
+            foreach (Lijn lijnverschuif in _lijnen)
+            {
+                lijnverschuif.PositieOpDeLijn++;
+            }
 
+            if (_lijnen.Last.Value.PositieOpDeLijn == 9){  
+            
+            }
+        }
+
+                public Lijn VerwijderLijnVanKabel()
+            {
+            if (_lijnen.Last.Value.PositieOpDeLijn == 9) 
+            {
+                
+                _lijnen.RemoveLast();
+
+                return _lijnen.Last;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+           string String = "";
+            foreach (Lijn lijn in _lijnen)
+            {
+                String += (lijn.PositieOpDeLijn + "|");
+            }
+            return (String.TrimEnd('|') + ".");
         }
     }
 }
---
+++ Waterskibaan/LijnenVoorraad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+
+namespace Waterskibaan
+{
+    class LijnenVoorraad
+    {
+
+        Queue<Lijn> _lijnen = new Queue<Lijn>();
+
+        public void LijnToevoegenAanRij(Lijn lijn)
+        {
+            _lijnen.Enqueue(lijn);
+        }
+
+        public Lijn VerwijderEersteLijn()
+        {
+            if(LijnenVoorraad == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Console.WriteLine($"De voorraad is {LijnenVoorraad} lijnen");
+            }
+        }
+
+
+        public int GetAantalLijnen()
+        {
+            foreach(int r in _lijnen)
+            {
+                Console.WriteLine(r);
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
